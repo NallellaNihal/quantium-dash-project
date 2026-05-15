b68431df7e6aead9534b7eb074ff017e1762df3a
NallellaNihal <NallellaNihal@users.noreply.github.com>
2026-05-15 15:25:01 +0530

    Completed Dash visualisation project with tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,30 +1,28 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app import app
 
 
-def test_app_title():
-    assert app.title is not None
-
-
-def test_layout_exists():
-    assert app.layout is not None
-
-
-def test_header_present():
+def test_header_is_present():
     layout_children = app.layout.children[0].children
-
     header = layout_children[0]
 
-    assert "Soul Foods" in header.children
+    assert header.children == "Soul Foods Pink Morsel Sales Visualiser"
 
 
-def test_graph_exists():
+def test_visualisation_is_present():
     layout_children = app.layout.children[0].children
-
     graph = layout_children[3]
 
     assert graph.id == "sales-line-chart"
+
+
+def test_region_picker_is_present():
+    layout_children = app.layout.children[0].children
+    region_picker_container = layout_children[2]
+    radio_items = region_picker_container.children[1]
+
+    assert radio_items.id == "region-filter"
